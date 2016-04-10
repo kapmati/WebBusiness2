923da3ad5a7f386497a21312721fa7fced502e64
Mateusz Kapron <kapmat77@gmail.com>
2016-04-10 11:46:24 +0200

    Successfully connect db

--- connectDB.php
+++ connectDB.php
@@ -7,36 +7,26 @@
 $servername = "mysql.agh.edu.pl:3306";
 $username = "kapmat";
 $password = "kapmatphp";
+$database = "kapmat";
 
+//connection to the database
+$connect = mysqli_connect($servername, $username, $password, $database)
+or die("Unable to connect to MySQL");
+echo "Connected to MySQL<br>";
 
-$connection = new mysqli($servername, $username, $password);
+$query = "SELECT * FROM Test";
+//execute the SQL query and return records
+$result = mysqli_query($connect, $query)
+    or die("Error query");
 
-if ($connection->connect_error) {
-    die("Connection failed: " . $connection->connect_error);
+//fetch tha data from the database
+while ($row = mysqli_fetch_array($result)) {
+    echo "<br>";
+    echo "ID:".$row{'ID'}." SYSTEM:".$row{'SYSTEM'}." BROWSER:".$row{'BROWSER'}.
+        " RESOLUTION:".$row{'RESOLUTION'}." DESKTOP:".$row{'DESKTOP'}."<br>";
 }
-echo "Connected successfully\n";
+//close the connection
+mysqli_close($connect);
 
-$sql = "SELECT * FROM Test";
-
-if ($connection->query($sql) === TRUE) {
-    echo "Success";
-} else {
-    echo " Error" . $connection->error;
-}
-
-
-//$sql = "CREATE TABLE MyGuests (
-//id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-//firstname VARCHAR(30) NOT NULL,
-//lastname VARCHAR(30) NOT NULL,
-//email VARCHAR(50),
-//reg_date TIMESTAMP
-//)";
-//
-//if ($connection->query($sql) === TRUE) {
-//    echo "Table MyGuests created successfully";
-//} else {
-//    echo "Error creating table: " . $connection->error;
-//}
 ?>
 
